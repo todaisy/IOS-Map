c4452d7a186ddc686fb2bf588e2cecba20ffa619
TODAISY <91897155+todaisy@users.noreply.github.com>
2022-12-12 14:16:33 +0300

    12.12.2022

---
+++ IOS-Maps/IOS-Map/IOS-Map/MapVC.swift
@@ -0,0 +1,29 @@
+//
+//  MapVC.swift
+//  IOS-Map
+//
+//  Created by Балаева Софья Сергеевна on 12/12/2022.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ IOS-Maps/IOS-Map/IOS-Map/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS-Map
+//
+//  Created by Балаева Софья Сергеевна on 12/12/2022.
+//
+
+import UIKit
+
+class LocationVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
